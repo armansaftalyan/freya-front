c89b70c22aa4501063bd4c34f95a358a6d42815d
arman <arman.saftalyan@gmail.com>
2026-02-17 06:41:08 +0400

    deploy test

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,8 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'beauty-salon-web',
-      script: 'node',
-      args: '.output/server/index.mjs',
+      script: '.output/server/index.mjs',
       instances: 'max',
       exec_mode: 'cluster',
       autorestart: true,
@@ -11,8 +10,10 @@ module.exports = {
       max_memory_restart: '512M',
       env: {
         NODE_ENV: 'production',
+        HOST: '0.0.0.0',
+        PORT: 3000,
+        NITRO_HOST: '0.0.0.0',
         NITRO_PORT: 3000,
-        NITRO_HOST: '127.0.0.1',
       },
     },
   ],
